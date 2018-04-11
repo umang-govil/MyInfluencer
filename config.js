eca1edf6ed65ddbfd5834306242230164504c79c
Himesh Jain <himeshjain13@gmail.com>
2018-04-11 10:36:44 +0530

    Remove: Config and Node Modules

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	"consumer_key": "Z4EWQILikkmi9E4qt0BjuMcQD",
-	"consumer_secret": "dhM5gANyETgEWxJv3o86bsoXKqZCgmsvfCfqaIe3imU6JnDshZ",
-	"access_token_key": "1693690603-oayZ6CKjUPk4X1IelTWpUHaqr3P3a2VoOSz8Uah",
-	"access_token_secret": "T7WG44QezYFmt62HGWUUGd7YdNZqmkcwXIFBiJfdKWdCb",
-	"port": process.env.PORT || 3000,
-	"database": "mongodb://127.0.0.1:27017/test"
-};
